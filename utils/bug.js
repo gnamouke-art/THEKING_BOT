519cdebefc3666aac7b15ba9d0361cf6b437ca18
gnamouke-art <evrardd85@gmail.com>
2026-04-19 13:09:05 +0000

    Create bug.js

---
+++ utils/bug.js
@@ -0,0 +1,24 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Channel",
+                    body: "★YANKEE†DEV†MONDIALE★",
+                    mediaType: 1,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029Vb7xYNO84OmK7L02eq3v`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
